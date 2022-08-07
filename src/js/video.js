d15841ec8212734e66023abdf5238aff430fd1a1
Zina <zinasaenko@hotmail.com>
2022-08-07 12:04:54 +0300

    :skull:

---
+++ src/js/video.js
@@ -0,0 +1,13 @@
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
